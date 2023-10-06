8dc60cb0ef9412d48c6975f3c554310d4ab1d684
NotZarifGithub <flacko.programming@gmail.com>
2023-10-06 14:00:27 +0800

    tiny update about section

--- src/components/feature/About.jsx
+++ src/components/feature/About.jsx
@@ -10,7 +10,7 @@ const fuggles = Fuggles({
 const About = () => {
   return (
     <section>
-      <div className='flex flex-col items-center justify-center lg:flex-row bg-[#36454f] border-b'>
+      <div className='flex flex-col items-center justify-center border-b lg:flex-row lg:py-[20px]'>
 
         {/* ABOUT IMAGE */}
         <div className="relative w-full h-[500px] lg:w-[600px]">
@@ -18,13 +18,13 @@ const About = () => {
         </div>
 
         {/* ABOUT CONTENT */}
-        <div className="flex flex-col items-center justify-center p-[30px] lg:p-[40px] max-w-[500px] md:max-w-[600px] lg:items-start">
+        <div className="flex flex-col items-center justify-center p-[30px] lg:p-[40px] max-w-[500px] md:max-w-[600px] lg:items-start text-white">
           <h1 className="items-center mb-5 text-2xl font-bold leading-none uppercase lg:text-4xl md:text-3xl">
             <span className={`${fuggles.className} text-6xl capitalize`}>explore</span><br />Our journey
           </h1>
 
           {/* ABOUT DESCRIPTION */}
-          <div className="flex flex-col text-sm font-semibold text-white">
+          <div className="flex flex-col text-sm font-semibold">
             <p>
               Welcome to our Italian restaurant, where pasta reigns supreme. At our culinary haven, we specialize in crafting 
               exquisite pasta dishes that transport you straight to the heart of Italy. Each plate is a symphony of flavors, 
