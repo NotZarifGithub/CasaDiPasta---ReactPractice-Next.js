35046373422fea844d5f2c067de07050ae8b7a2c
NotZarifGithub <flacko.programming@gmail.com>
2023-10-07 16:58:36 +0800

    added googlmap components and booking section

--- src/components/feature/Booking.jsx
+++ src/components/feature/Booking.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import GoogleMapsLoader from './GoogleMapsLoader';
 
 const Booking = () => {
   return (
-    <div>Booking</div>
+    <section className='md:py-[100px] border-b'>
+      <div className='max-w-[700px] mx-auto flex flex-col'>
+
+        {/* GOOGLE MAPS */}
+        <GoogleMapsLoader />
+        <div className='text-white p-[40px]'>
+          <h1 className="justify-center text-center mb-5 text-2xl font-bold leading-none uppercase md:text-3xl py-[20px]">
+            Book a table
+          </h1>
+
+          {/* BOOKING SECTION */}
+          <div className='flex flex-col bg-black/40 p-[20px]'>
+            <div>
+              <input type="text" placeholder='First Name' className='bg-black p-[10px] m-[10px]'/>
+              <input type="text" placeholder='Last Name' className='bg-black p-[10px] m-[10px]' />
+            </div>
+            <div>
+              <input type="datetime-local" placeholder='Date' className='bg-black p-[10px] m-[10px]'/>
+              <input type="text" placeholder='Phone' className='bg-black p-[10px] m-[10px]'/>
+            </div>
+            <div>
+              <input type="text" placeholder="Message" className='bg-black p-[10px] m-[10px]'/>
+              <button>
+                <input type="submit" className='bg-black p-[10px] m-[10px]'/>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
