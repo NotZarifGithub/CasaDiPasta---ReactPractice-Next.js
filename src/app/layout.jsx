30dff9e7b4f29aa6e4c6a675df8b6cedc6f3321d
NotZarifGithub <flacko.programming@gmail.com>
2023-10-10 12:39:04 +0800

    favicon.ico fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,22 +2,21 @@ import Navbar from '@/components/common/Navbar'
 import './globals.css'
 import { Montserrat } from 'next/font/google'
 import Footer from '@/components/common/Footer'
-import Head from 'next/head'
+import head from 'next/head'
 
 const monstserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Casa Di Pasta',
-  description: `Casa Di Pasta is a restaurant in the heart of the city of Rome, Italy. We offer a 
-                variety of dishes, from pasta to pizza, and even desserts.`,
+  description: `Casa Di Pasta is a restaurant in the heart of the city of Rome, Italy. We offer a variety of dishes, from pasta to pizza, and even desserts.`,
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
+      </head>
       <body className={monstserrat.className}>
         <Navbar />
           {children}
